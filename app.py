990f238346ba603eb29e42615f31a6405d42ded2
soundreaper <soundreaper@gmail.com>
2019-12-15 22:00:43 -0800

    add additional routes and change header into navbar with dropdown and login/register

--- app.py
+++ app.py
@@ -7,15 +7,25 @@ def index():
     """Index page of the application"""
     return render_template('index.html')
 
+@app.route('/adobe')
+def adobe():
+    """Page for editing tutorials using Adobe Software"""
+    return render_template('adobe.html')
+
 @app.route('/sony_vegas')
 def sony_vegas():
     """Page for editing tutorials using Sony Vegas"""
     return render_template('sony_vegas.html')
 
-@app.route('/adobe')
-def adobe():
-    """Page for editing tutorials using Adobe Software"""
-    return render_template('adobe.html')
+@app.route('/login')
+def login():
+    """Page for logging in"""
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    """Page for registering"""
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
